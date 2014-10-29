ae9876265243a7089b45d770b03bc6bf0ae2febb
leungwensen <leungwensen@gmail.com>
2014-10-29 19:51:53 +0800

    init commit

---
+++ bin/deptree/java.js
@@ -0,0 +1,11 @@
+/* jshint strict: true, undef: true, unused: true, node: true */
+// /* global */
+"use strict";
+
+var analyse = function(option) {
+    
+};
+
+module.exports = {
+    analyse: analyse
+};
