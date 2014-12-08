5b9c7c6448adf257720d3602a124422e05aa0f95
绝云 <wensen.lws@alibaba-inc.com>
2014-12-08 23:30:56 +0800

    bugfix #13

--- js/demo.js
+++ js/demo.js
@@ -1,5 +1,8 @@
+/* jshint strict: true, undef: true, unused: true */
+/* global define, $, d3, dagreD3, pastry, CryptoJS, alert */
 
 define('jquery', function () {
+    'use strict';
     return $;
 });
 define('d3'       , d3       );
@@ -14,10 +17,17 @@ require([
     $,
     depTree
 ) {
+    'use strict';
+
     $.ajaxSetup ({
         cache: false
     });
     $.get('json/demoTree.json', function(tree) {
+        if (!tree.nodes || !tree.nodes.length) {
+            // #13 bugfix 结果为空的时候不作进一步画图处理
+            alert('no result!');
+            return;
+        }
         depTree.init(tree).draw(tree);
     });
 });
